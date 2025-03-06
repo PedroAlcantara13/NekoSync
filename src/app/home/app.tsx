ae2d4229b15852196ac1299a77a36b12e5c222b1
Pedro Alcantara <alcantarap37@gmail.com>
2025-03-06 01:54:12 -0300

    Algumas aterações Layout e Header

--- src/app/home/app.tsx
+++ src/app/home/app.tsx
@@ -1,111 +1,114 @@
 import { useEffect, useRef } from 'react';
 
-const Carousel = () => {
-    const nextDom = useRef<HTMLButtonElement | null>(null);
-    const prevDom = useRef<HTMLButtonElement | null>(null);
-    const carouselDom = useRef<HTMLDivElement | null>(null);
-    const sliderDom = useRef<HTMLDivElement | null>(null);
-    const thumbnailBorderDom = useRef<HTMLDivElement | null>(null);
-    const timeDom = useRef<HTMLDivElement | null>(null);
-
-    const timeRunning = 3000;
-    const timeAutoNext = 8000;
-
-    let runTimeOut: ReturnType<typeof setTimeout>;
-    let runNextAuto: ReturnType<typeof setTimeout>;
-
-    const showSlider = (type: 'next' | 'prev') => {
-        const sliderItemsDom = sliderDom.current?.querySelectorAll('.carousel .list .item');
-        const thumbnailItemsDom = thumbnailBorderDom.current?.querySelectorAll('.carousel .thumbnail .item');
-
-        if (sliderItemsDom && thumbnailItemsDom) {
-            if (type === 'next') {
-                sliderDom.current?.appendChild(sliderItemsDom[0]);
-                thumbnailBorderDom.current?.appendChild(thumbnailItemsDom[0]);
-                carouselDom.current?.classList.add('next');
-            } else {
-                sliderDom.current?.prepend(sliderItemsDom[sliderItemsDom.length - 1]);
-                thumbnailBorderDom.current?.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-                carouselDom.current?.classList.add('prev');
-            }
-
-            clearTimeout(runTimeOut);
-            runTimeOut = setTimeout(() => {
-                carouselDom.current?.classList.remove('next');
-                carouselDom.current?.classList.remove('prev');
-            }, timeRunning);
-
-            clearTimeout(runNextAuto);
-            runNextAuto = setTimeout(() => {
-                nextDom.current?.click();
-            }, timeAutoNext);
-        }
-    };
-
-    useEffect(() => {
-        nextDom.current?.addEventListener('click', () => showSlider('next'));
-        prevDom.current?.addEventListener('click', () => showSlider('prev'));
-
-        runNextAuto = setTimeout(() => {
-            nextDom.current?.click();
-        }, timeAutoNext);
-
-        return () => {
-            clearTimeout(runNextAuto);
-            clearTimeout(runTimeOut);
-        };
-    }, []);
-
-    const car = [
-        { id: 1, author: "NekoSync", title: "Design", topic: "Outono", desc: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", name: "Name Slider", des: "Description", img: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/04/Winter-2021-10-Best-Anime-Of-The-Season-Ranked-featured-image.jpg" },
-        { id: 2, author: "NekoSync2", title: "Design2", topic: "Outono", desc: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", name: "Name Slider", des: "Description", img: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/06/summer-2023-anime-complete-guide.jpg" },
-    ]
-
+const Cont = () => {
     return (
-<div className="carousel" ref={carouselDom}>
-                <div ref={sliderDom} className="list">
-                {car.map((car, index) => (
-                    <div key={index} className="item">
-                        <img src={car.img} alt="" />
-                        <div className="content">
-                            <div className="author">{car.author}</div>
-                            <div className="title">{car.title}</div>
-                            <div className="topic">{car.topic}</div>
-                            <div className="des">
-                            {car.desc}
-                            </div>
-                            <div className="buttons">
-                                <button className="bg-primary">SEE MORE</button>
-                                <button className="border border-inherit">SUBSCRIBE</button>
+    <div className="container mx-auto">
+        <div className="row">
+            <div className="col-span-12">
+                <div className="page-content bg-base-300">
+
+                    <div className="min-h-96 rounded-3xl py-20 px-14 bg-base-100">
+                        <div className="">
+                            <div className="">
+                                <div>
+                                    <h6 className='text-xl font-normal mb-6'>Welcome To Cyborg</h6>
+                                    <h4 className='text-5xl uppercase mb-6'><em>Browse</em> Our Popular Games Here</h4>
+                                    <div className="main-button">
+                                        <button>Browse Now</button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                ))}
-                </div>
 
-                <div ref={thumbnailBorderDom} className="thumbnail">
-                    {car.map((car, index) => (
-                    <div key={index} className="item">
-                       <img src={car.img} alt="" />
-                        <div className="content">
-                            <div className="title">
-                                {car.name}
-                            </div>
-                            <div className="description">
-                                {car.des}
+                    <div className="mt-14 p-8 bg-base-100 rounded-3xl">
+                        <div className="flex flex-wrap">
+                            <div className="w-full lg:w-full">
+
+                                <div className="heading-section">
+                                <h4><em>Most Popular</em> Right Now</h4>
+                                </div>
+
+                                <div className="flex flex-wrap">
+                                    <div className="lg:w-1/4 sm:w-1/2 p-4">
+                                        <div className="bg-base-300 py-8 px-4 rounded-3xl">
+                                            <div className='bg-blue-600 rounded-3xl'>.</div>
+                                            <h4 className='text-base'>Fortnite<span className='text-slate-600 font-normal'>Sandbox</span></h4>
+                                            <ul className='float-right'>
+                                                <li className='text-right text-sm'><i className="fa fa-star"></i> 4.8</li>
+                                                <li className='text-right text-sm'><i className="fa fa-download"></i> 2.3M</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                    <div className="lg:w-1/4 sm:w-1/2 p-4">
+                                        <div className="bg-base-300 py-8 px-4 rounded-3xl">
+                                            <div className='bg-blue-600 rounded-3xl'>.</div>
+                                            <h4 className='text-base'>Fortnite<span className='text-slate-600 font-normal'>Sandbox</span></h4>
+                                            <ul className='float-right'>
+                                                <li className='text-right text-sm'><i className="fa fa-star"></i> 4.8</li>
+                                                <li className='text-right text-sm'><i className="fa fa-download"></i> 2.3M</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                    <div className="lg:w-1/4 sm:w-1/2 p-4">
+                                        <div className="bg-base-300 py-8 px-4 rounded-3xl">
+                                            <div className='bg-blue-600 rounded-3xl'>.</div>
+                                            <h4 className='text-base'>Fortnite<span className='text-slate-600 font-normal'>Sandbox</span></h4>
+                                            <ul className='float-right'>
+                                                <li className='text-right text-sm'><i className="fa fa-star"></i> 4.8</li>
+                                                <li className='text-right text-sm'><i className="fa fa-download"></i> 2.3M</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                    <div className="lg:w-1/4 sm:w-1/2 p-4">
+                                        <div className="bg-base-300 py-8 px-4 rounded-3xl">
+                                            <div className='bg-blue-600 rounded-3xl'>.</div>
+                                            <h4 className='text-base'>Fortnite<span className='text-slate-600 font-normal'>Sandbox</span></h4>
+                                            <ul className='float-right'>
+                                                <li className='text-right text-sm'><i className="fa fa-star"></i> 4.8</li>
+                                                <li className='text-right text-sm'><i className="fa fa-download"></i> 2.3M</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                    <div className="lg:w-1/4 sm:w-1/2 p-4">
+                                        <div className="bg-base-300 py-8 px-4 rounded-3xl">
+                                            <div className='bg-blue-600 rounded-3xl'>.</div>
+                                            <h4 className='text-base'>Fortnite<span className='text-slate-600 font-normal'>Sandbox</span></h4>
+                                            <ul className='float-right'>
+                                                <li className='text-right text-sm'><i className="fa fa-star"></i> 4.8</li>
+                                                <li className='text-right text-sm'><i className="fa fa-download"></i> 2.3M</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                    <div className="lg:w-1/4 sm:w-1/2 p-4">
+                                        <div className="bg-base-300 py-8 px-4 rounded-3xl">
+                                            <div className='bg-blue-600 rounded-3xl'>.</div>
+                                            <h4 className='text-base'>Fortnite<span className='text-slate-600 font-normal'>Sandbox</span></h4>
+                                            <ul className='float-right'>
+                                                <li className='text-right text-sm'><i className="fa fa-star"></i> 4.8</li>
+                                                <li className='text-right text-sm'><i className="fa fa-download"></i> 2.3M</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                    
+
+                                </div>
+
                             </div>
                         </div>
                     </div>
-                    ))} 
-                </div>
 
-                <div className="arrows">
-                    <button ref={prevDom} id="prev">{"<"}</button>
-                    <button ref={nextDom} id="next">{">"}</button>
                 </div>
-                <div ref={timeDom} className="time"></div>
             </div>
+        </div>
+    </div>    
     );
 };
 
-export default Carousel;
+export default Cont;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,24 @@
 export function Header(){
     return (
-  <div className="navbar bg-base-300">
-    <div className="navbar-start">
-    </div>
-  
-    <div className="navbar-center">
-      <a className="btn btn-ghost text-xl text-primary">NekoSync</a>
-    </div>
+    <div className="bg-base-300">
+      <div className="navbar container mx-auto">
+        <div className="navbar-start">
+          <a className="btn btn-ghost text-xl text-primary">NekoSync</a>
+        </div>
+      
+        <div className="navbar-center">
+        </div>
 
-    <div className="navbar-end">
-
+        <div className="navbar-end">
+          <ul className="nav flex">
+            <li><a href="" className="active">Home</a></li>
+            <li><a href="">Browse</a></li>
+            <li><a href="">Details</a></li>
+            <li><a href="">Streams</a></li>
+            <li><a href="">Profile</a></li>
+          </ul> 
+        </div>
+      </div>
     </div>
-</div>
     )
 }
